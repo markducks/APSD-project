30b44fe53a1e2ba390da0c989b097d4b463b816c
Marco Duca <dcumrc02a07d086r@studenti.unical.it>
2023-11-10 14:09:34 +0100

    Borders and corner send now works right

--- CA.cpp
+++ CA.cpp
@@ -15,6 +15,7 @@ MPI_Datatype columnType, rowType, cornerType;
 MPI_Comm cartComm;
 int* readM;
 int* writeM;
+int dims[2];
 int rowsPerProc, colsPerProc;
 
 void testInit(){
@@ -25,16 +26,6 @@ void testInit(){
     }
 }
 
-void printTest(){
-    printf("RANK : %d\n", rank);
-    for(int i=0; i<rowsPerProc+2; i++){
-        for(int j=0; j<colsPerProc+2; j++){
-            printf("%d ", readM[v(i,j)]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
 
 void init(){
     for(int i=0; i<rowsPerProc+2; i++){
@@ -44,7 +35,7 @@ void init(){
     }
 }
 
-void initCA(){
+void initRandomCA(){
     //Random initialization of Game Of Life
     for(int i=1; i<rowsPerProc+1; i++){
         for(int j=1; j<colsPerProc+1; j++){
@@ -53,22 +44,22 @@ void initCA(){
     }
 }
 
-void swap(){}
 
 inline void send(){
     MPI_Request req;
     //Send the borders to the neighbors
-    MPI_Send(&readM[v(1,1)], 1, rowType, rankUp, 17, cartComm);
-    MPI_Send(&readM[v(rowsPerProc,1)], 1, rowType, rankDown, 18, cartComm);
-    MPI_Send(&readM[v(1,1)], 1, columnType, rankLeft, 19, cartComm);
-    MPI_Send(&readM[v(1,colsPerProc)], 1, columnType, rankRight, 20, cartComm);
+    MPI_Send(&readM[v(1,0)], 1, rowType, rankUp, 17, cartComm);
+    MPI_Send(&readM[v(rowsPerProc,0)], 1, rowType, rankDown, 18, cartComm);
+    MPI_Send(&readM[v(0,1)], 1, columnType, rankLeft, 19, cartComm);
+    MPI_Send(&readM[v(0,colsPerProc)], 1, columnType, rankRight, 20, cartComm);
 
+    
     //Send the corners to the diagonal neighbors
     MPI_Send(&readM[v(1,1)], 1, cornerType, rankDiagonalUpLeft, 21, cartComm);
     MPI_Send(&readM[v(1,colsPerProc)], 1, cornerType, rankDiagonalUpRight, 22, cartComm);
     MPI_Send(&readM[v(rowsPerProc,1)], 1, cornerType, rankDiagonalDownLeft, 23, cartComm);
     MPI_Send(&readM[v(rowsPerProc,colsPerProc)], 1, cornerType, rankDiagonalDownRight, 24, cartComm);
-
+    
 }
 
 inline void recv(){
@@ -78,12 +69,14 @@ inline void recv(){
     MPI_Recv(&readM[v(0,0)],1,rowType,rankUp,18,cartComm,&status);
     MPI_Recv(&readM[v(0,colsPerProc+1)],1,columnType,rankRight,19,cartComm,&status);
     MPI_Recv(&readM[v(0,0)],1,columnType,rankLeft,20,cartComm,&status);
-
+    
+    
     //Receive the diagonals from the neighbors
-    MPI_Recv(&readM[v(rowsPerProc+2,colsPerProc+2)],1,cornerType,rankDiagonalDownRight,21,cartComm,&status);
-    MPI_Recv(&readM[v(rowsPerProc+2,0)],1,cornerType,rankDiagonalDownLeft,22,cartComm,&status);
-    MPI_Recv(&readM[v(rowsPerProc+2,colsPerProc+2)],1,cornerType,rankDiagonalUpRight,23,cartComm,&status);
+    MPI_Recv(&readM[v(rowsPerProc+1,colsPerProc+1)],1,cornerType,rankDiagonalDownRight,21,cartComm,&status);
+    MPI_Recv(&readM[v(rowsPerProc+1,0)],1,cornerType,rankDiagonalDownLeft,22,cartComm,&status);
+    MPI_Recv(&readM[v(0,colsPerProc+1)],1,cornerType,rankDiagonalUpRight,23,cartComm,&status);
     MPI_Recv(&readM[v(0,0)],1,cornerType,rankDiagonalUpLeft,24,cartComm,&status);
+    
 }
 
 inline void transFuncInside(){}
@@ -92,6 +85,8 @@ inline void transFuncBorders(){}
 
 inline void transFunc(){}
 
+void swap(){}
+
 void printCoords(MPI_Comm cartComm){
     int myCoords[2] = {0, 0};
     MPI_Cart_coords(cartComm, rank, 2, myCoords);
@@ -99,21 +94,37 @@ void printCoords(MPI_Comm cartComm){
     printf("Rank: %d, coords: (%d,%d)\n rankLeft: %d, rankRight: %d, rankUp: %d, rankDown: %d\n rankDiagonalUpLeft: %d, rankDiagonalUpRight: %d, rankDiagonalDownLeft: %d, rankDiagonalDownRight: %d\n\n", rank, myCoords[0], myCoords[1], rankLeft, rankRight, rankUp, rankDown, rankDiagonalUpLeft, rankDiagonalUpRight, rankDiagonalDownLeft, rankDiagonalDownRight);
 }
 
-//MAIN
-int main(int argc, char *argv[]){
+inline void printGather(){
+    int* gatherBuffer = NULL;
+    if(rank == 0){
+        gatherBuffer = new int[nProc * (rowsPerProc+2) * (colsPerProc+2)];
+    }
 
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &nProc);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Gather(readM, (rowsPerProc+2) * (colsPerProc+2), MPI_INT, gatherBuffer, (rowsPerProc+2) * (colsPerProc+2), MPI_INT, 0, MPI_COMM_WORLD);
 
-    int dims[2] = {0, 0}; 
+    if (rank == 0) {
+        for (int i = 0; i < nProc; i++) {
+            printf("Matrix from rank %d:\n", i);
+            for (int j = 0; j < (rowsPerProc+2); j++) {
+                for (int k = 0; k < (colsPerProc+2); k++) {
+                    printf("%d ", gatherBuffer[i * (rowsPerProc+2) * (colsPerProc+2) + j * (colsPerProc+2) + k]);
+                }
+                printf("\n");
+            }
+            printf("\n");
+        }
+        delete[] gatherBuffer;
+    }
+}
+
+inline void rankDiscovering(){
+    dims[0] = dims[1] = 0;
     MPI_Dims_create(nProc, 2, dims); 
     int periods[2] = {1,1};
     MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &cartComm);
     MPI_Cart_shift(cartComm, 0, 1, &rankUp, &rankDown);
     MPI_Cart_shift(cartComm, 1, 1, &rankLeft, &rankRight);
 
-    //Calculate the ranks of the processes in the diagonal
     int coords[2];
     MPI_Cart_coords(cartComm, rank, 2, coords); 
     coords[0] = (coords[0] - 1 + dims[0]) % dims[0];
@@ -125,31 +136,38 @@ int main(int argc, char *argv[]){
     MPI_Cart_rank(cartComm, coords, &rankDiagonalDownRight);
     coords[1] = (coords[1] - 2 + dims[1]) % dims[1];
     MPI_Cart_rank(cartComm, coords, &rankDiagonalDownLeft);
+}
 
-    //printCoords(cartComm); //Todo: Remove
+//MAIN
+int main(int argc, char *argv[]){
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &nProc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        
+    rankDiscovering();
 
     rowsPerProc = NROWS/dims[0];
     colsPerProc = NCOLS/dims[1];
     readM = new int[(rowsPerProc+2)*(colsPerProc+2)];
     writeM = new int[(rowsPerProc+2)*(colsPerProc+2)];
 
-    MPI_Type_vector(rowsPerProc+2, 1, colsPerProc+2, MPI_INT, &columnType); //NROWS+4 or NROWS?
+    MPI_Type_vector(rowsPerProc+2, 1, colsPerProc+2, MPI_INT, &columnType);
     MPI_Type_commit(&columnType);
     MPI_Type_vector(colsPerProc+2, 1, 1, MPI_INT, &rowType);
     MPI_Type_commit(&rowType);
-    MPI_Type_vector(1, 1, 1, MPI_INT, &cornerType); //Useless(?) but created for consistency
+    MPI_Type_vector(1, 1, 1, MPI_INT, &cornerType);
     MPI_Type_commit(&cornerType);
 
-    //init();
-    testInit();
+    init();
 
-    //initCA();
+    initRandomCA();
 
     for(int i = 0; i < NGENS; i++){
         send();
         //transFuncInside();
         recv();
-        printTest();
+        printGather();
         //transFuncBorders();
         //Allegro here(?)
         //swap();
